8c6233b38e4b3207bbd4122217430f64e235f059
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-20 01:09:23 +0200

    Update index.js

--- data/index.js
+++ data/index.js
@@ -3,12 +3,69 @@ export const produts =
         {
             id: 1,
             name: "Cachorro-quente",
-            Image: require('../assets/imgs/Rectangle5.png'),
+            Image: require('../assets/imgs/comida.png'),
+            price: 1200,
             description: "Um pãozinho recheado com uma salsicha, geralmente acompanhado de molho de tomate, mostarda, ketchup, cebola, e outras coberturas.",
         }, {
             id: 2,
-            name: "",
+            name: "Hambúrguer",
             Image: require('../assets/imgs/Rectangle5.png'),
-            description: "",
+            price: 500,
+            description: "Carne moída grelhada ou frita, geralmente servida dentro de um pão com diversos acompanhamentos, como alface, tomate, queijo, e molhos.",
         }, {
-      
+            id: 3,
+            name: "Peixe e batatas fritas",
+            Image: require('../assets/imgs/comida1.png'),
+            price: 200,
+            description: "Peixe empanado e frito, servido com batatas cortadas em tiras e também fritas. Pode ser acompanhado de molho tártaro.",
+        }
+        , {
+            id: 3,
+            name: "Peixe fritas",
+            Image: require('../assets/imgs/comida2.png'),
+            price: 700,
+            description: "Peixe empanado e frito, servido com batatas cortadas em tiras e também fritas. Pode ser acompanhado de molho tártaro.",
+        }
+    ]
+
+export const OthersProduts =
+    [
+        {
+            id: 1,
+            name: "Sushi",
+            Image: require('../assets/imgs/Rectangle9.png'),
+            description: "Uma variedade de ",
+        }, {
+            id: 2,
+            name: "Frango ao curry",
+            Image: require('../assets/imgs/Rectangle6.png'),
+            description: "Pedaços de frango .",
+        }, {
+            id: 3,
+            name: "Salada Caesar",
+            Image: require('../assets/imgs/Rectangle8.png'),
+            description: "Uma salada feita com ",
+        }
+    ]
+
+export const AnimalCategory =
+    [
+        {
+            id: 1,
+            name: 'Cat',
+        }, {
+            id: 2,
+            name: 'Dog',
+        },
+        {
+            id: 3,
+            name: 'Pug',
+        },
+        {
+            id: 4,
+            name: 'Robbit',
+        }, {
+            id: 5,
+            name: 'turtle',
+        }
+    ]
