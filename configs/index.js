5bacfc52bb14dfa1af6c4415af0686259cc740ea
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-20 01:09:16 +0200

    Update index.js

--- configs/index.js
+++ configs/index.js
@@ -1,6 +1,15 @@
 export const PRIMARY = "#2B2C40";
 export const GRAY = "#667C8A";
-export const ROSA = "#FEF7F5";
+export const ROSA = "#E7A491";
 export const VERDE = "#F7FFF4";
 export const AZUL = "#EAF1FB";
+export const LIGHT = "#FFF";
 export const SPACING = 5;
+export const user = {
+    name: "Arnaldo Tomo",
+    profile: require('../assets/imgs/Main.png'),
+};
+
+export const imagOneboard = require('../assets/imgs/image1.png');
+export const IconHome = require('../assets/icon/ic_round-dashboard.png');
+export const IconGroup = require('../assets/icon/Group.png');
