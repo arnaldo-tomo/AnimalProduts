c098c78d60580b1d129a564f2e12dd5fd2e69187
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-19 19:37:40 +0200

    Initial commit
    Generated by create-expo-app 2.0.3.

---
+++ configs/index.js
@@ -0,0 +1,6 @@
+export const PRIMARY = "#2B2C40";
+export const GRAY = "#667C8A";
+export const ROSA = "#FEF7F5";
+export const VERDE = "#F7FFF4";
+export const AZUL = "#EAF1FB";
+export const SPACING = 5;
---
+++ data/index.js
@@ -0,0 +1,14 @@
+export const produts =
+    [
+        {
+            id: 1,
+            name: "Cachorro-quente",
+            Image: require('../assets/imgs/Rectangle5.png'),
+            description: "Um pãozinho recheado com uma salsicha, geralmente acompanhado de molho de tomate, mostarda, ketchup, cebola, e outras coberturas.",
+        }, {
+            id: 2,
+            name: "",
+            Image: require('../assets/imgs/Rectangle5.png'),
+            description: "",
+        }, {
+      
