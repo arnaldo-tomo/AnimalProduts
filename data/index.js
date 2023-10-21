b157b3872a243e74a35ac62acde44a9782cb83c1
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-21 13:01:32 +0200

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,3 +1,5 @@
+import { AZUL, PRIMARY, ROSA, VERDE } from '../configs'
+
 export const produts =
     [
         {
@@ -5,26 +7,56 @@ export const produts =
             name: "Cachorro-quente",
             Image: require('../assets/imgs/comida.png'),
             price: 1200,
+            gm: 100,
+
             description: "Um pãozinho recheado com uma salsicha, geralmente acompanhado de molho de tomate, mostarda, ketchup, cebola, e outras coberturas.",
         }, {
             id: 2,
             name: "Hambúrguer",
             Image: require('../assets/imgs/Rectangle5.png'),
             price: 500,
+            gm: 200,
+
             description: "Carne moída grelhada ou frita, geralmente servida dentro de um pão com diversos acompanhamentos, como alface, tomate, queijo, e molhos.",
         }, {
             id: 3,
             name: "Peixe e batatas fritas",
             Image: require('../assets/imgs/comida1.png'),
             price: 200,
+            gm: 300,
+
             description: "Peixe empanado e frito, servido com batatas cortadas em tiras e também fritas. Pode ser acompanhado de molho tártaro.",
         }
         , {
-            id: 3,
+            id: 4,
             name: "Peixe fritas",
             Image: require('../assets/imgs/comida2.png'),
             price: 700,
+            gm: 400,
             description: "Peixe empanado e frito, servido com batatas cortadas em tiras e também fritas. Pode ser acompanhado de molho tártaro.",
+        }
+    ]
+
+export const PaymentMZ =
+    [
+        {
+            id: 1,
+            prince: 4,
+            name: "M-Pesa ",
+            Image: require('../assets/imgs/mpesalogo.png'),
+            description: "Uma variedade de ",
+        }, {
+            id: 2,
+            prince: 0,
+            name: "E-Mola ",
+            Image: require('../assets/imgs/emola.png'),
+            description: "Pedaços de frango .",
+        }, {
+            id: 3,
+            prince: 0,
+            name: "MKesh ",
+            Image: require('../assets/imgs/mkesh.png'),
+            description: "Uma salada feita com ",
         }
     ]
 
@@ -32,16 +64,25 @@ export const OthersProduts =
     [
         {
             id: 1,
+            prince: 100.80,
+            gm: 100,
+            color: AZUL,
             name: "Sushi",
             Image: require('../assets/imgs/Rectangle9.png'),
             description: "Uma variedade de ",
         }, {
             id: 2,
+            prince: 200.80,
+            gm: 200,
+            color: VERDE,
             name: "Frango ao curry",
             Image: require('../assets/imgs/Rectangle6.png'),
             description: "Pedaços de frango .",
         }, {
             id: 3,
+            prince: 300.80,
+            gm: 400,
+            color: ROSA,
             name: "Salada Caesar",
             Image: require('../assets/imgs/Rectangle8.png'),
             description: "Uma salada feita com ",
@@ -69,3 +110,4 @@ export const AnimalCategory =
             name: 'turtle',
         }
     ]
+
