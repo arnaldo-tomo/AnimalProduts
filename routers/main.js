4bf67dcd1d1da0afe67e486b3cc336bf87ea5192
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-21 13:01:55 +0200

    Update main.js

--- routers/main.js
+++ routers/main.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Oneboarding } from '../screens/oneboarding';
+import { Product } from '../screens/Product';
+import { Payment } from '../screens/Payment';
 import TabsNv from './tabs';
 
 
@@ -15,6 +16,8 @@ function Main() {
             <Stack.Navigator>
                 <Stack.Screen name="Oneboarding" component={Oneboarding} options={{ headerShown: false }} />
                 <Stack.Screen name="Tabs" component={TabsNv} options={{ headerShown: false }} />
+                <Stack.Screen name="Product" component={Product} options={{ headerShown: false }} />
+                <Stack.Screen name="Payment" component={Payment} options={{ headerShown: false }} />
             </Stack.Navigator>
         </NavigationContainer>
     );
