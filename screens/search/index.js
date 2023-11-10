1fdd9730b773b56871630149a60fb3795de1ac04
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-10 14:42:21 +0200

    Update index.js

--- screens/search/index.js
+++ screens/search/index.js
@@ -19,17 +19,22 @@ export const Search = () => {
                 <Image source={user.profile} style={{ width: 50, height: 50, borderRadius: 15 }} />
             </View>
 
-            <View style={{
-                backgroundColor: PRIMARY, width: width - SPACING * 3, height: 50,
-                padding: SPACING * 2, marginLeft: SPACING * 2, borderRadius: SPACING, flexDirection: 'row', justifyContent: 'space-between'
-            }}>
-                <TouchableOpacity style={{ backgroundColor: PRIMARY }}>
-                    <Ionicons name="search" color={LIGHT} size={20} />
-                </TouchableOpacity>
-                <TextInput value={busca}  onChangeText={(text) => setBusca(text)} placeholder="Find the best product for your pet" placeholderTextColor={LIGHT} />
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
 
-                <Ionicons name="filter" color={LIGHT} size={20} />
+                <View style={{
+                    backgroundColor: PRIMARY, width: width - SPACING * 15, height: 50,
+                    padding: SPACING * 2, marginLeft: SPACING * 2, borderRadius: SPACING, flexDirection: 'row',
+                }}>
+                    <Ionicons name="search" color={LIGHT} size={20} />
+                    <TextInput value={busca} style={{ color: LIGHT }} onChangeText={(text) => setBusca(text)} placeholder="Find the best product for your pet" placeholderTextColor={LIGHT} />
+
+                </View>
+
+                <TouchableOpacity style={{ backgroundColor: PRIMARY, borderRadius: SPACING, padding: SPACING * 2, marginLeft }}>
+                    <Ionicons name="filter" color={LIGHT} size={20} />
+                </TouchableOpacity>
             </View>
+
             <View style={{ justifyContent: 'center' }}>
                 <Text>Resulatdo:{busca}</Text>
             </View>
