bc3cbb6b42f0e7a75b942a3caceb111a812c0cd1
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-11 22:29:07 +0200

    Create index.js

---
+++ screens/menu/index.js
@@ -0,0 +1,10 @@
+import { Text } from "react-native"
+import { View } from "react-native"
+import { LIGHT } from "../../configs/index";
+export const Menu = () => {
+    return (
+        <View style={{ flex: 1, backgroundColor: LIGHT }}>
+            <Text>Menu Babo</Text>
+        </View>
+    )
+}
