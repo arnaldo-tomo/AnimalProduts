5d2c62d427e5283531b0b042f15881fe368f75f7
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-11 08:23:09 +0200

    Update index.js

--- screens/search/index.js
+++ screens/search/index.js
@@ -15,7 +15,7 @@ export const Search = ({ navigation }) => {
     const [activeCategria, setActiveCategria] = useState(1);
     const pesquisado = produts.filter((data) => data.name.toLowerCase().includes(busca.toLowerCase()))
     return (
-        <SafeAreaView style={{ backgroundColor: LIGHT, flex: 1 }}>
+        <View style={{ backgroundColor: LIGHT, flex: 1., bottom: 'auto' }}>
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 30, padding: 15, }}>
                 <Ionicons name="grid" color={PRIMARY} size={20} />
                 <Text style={{ fontWeight: 'bold', fontSize: 15 }}>Explore</Text>
@@ -47,26 +47,27 @@ export const Search = ({ navigation }) => {
                     </ScrollView>
                 )}
             </View>
-            {pesquisado.map((data) =>
+            <ScrollView>
+                {pesquisado.map((data) =>
+                    <TouchableOpacity key={data.id} style={{
+                        padding: SPACING * 2,
+                        borderRadius: SPACING, marginTop: SPACING, backgroundColor:
+                            LIGHT, width: width - 18, height: 120, elevation: 2,
+                        marginLeft: SPACING * 2, marginRight: SPACING * 2,
+                        flexDirection: 'row'
+                    }} onPress={() => navigation.navigate('Product', data)}>
 
-                <TouchableOpacity key={data.id} style={{
-                    padding: SPACING * 2,
-                    borderRadius: SPACING, marginTop: SPACING, backgroundColor:
-                        LIGHT, width: width - 18, height: 120, elevation: 2,
-                    marginLeft: SPACING * 2, marginRight: SPACING * 2,
-                    flexDirection: 'row'
-                }} onPress={() => navigation.navigate('Product', data)}>
-
-                    <View>
-                        <Image source={data.Image} style={{ width: 100, height: 100 }} />
-                    </View>
-                    <View>
-                        <Text style={{ fontWeight: 'bold' }}>{data.name}</Text>
-                        <Text style={{ color: GRAY, width: 240, fontWeight: 'normal' }}>{data.description}</Text>
-                        <Text style={{ fontWeight: 'bold', color: GRAY }}>{data.price}.00MZN</Text>
-                    </View>
-                </TouchableOpacity>
-            )}
-        </SafeAreaView>
+                        <View>
+                            <Image source={data.Image} style={{ width: 100, height: 100 }} />
+                        </View>
+                        <View>
+                            <Text style={{ fontWeight: 'bold' }}>{data.name}</Text>
+                            <Text style={{ color: GRAY, width: 240, fontWeight: 'normal' }}>{data.description}</Text>
+                            <Text style={{ fontWeight: 'bold', color: GRAY }}>{data.price}.00MZN</Text>
+                        </View>
+                    </TouchableOpacity>
+                )}
+            </ScrollView>
+        </View>
     )
 }
