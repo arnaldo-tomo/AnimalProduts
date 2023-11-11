396e0bd25fccf223e0008285c453fedef2050c9c
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-11 09:16:44 +0200

    Update index.js

--- screens/search/index.js
+++ screens/search/index.js
@@ -15,59 +15,66 @@ export const Search = ({ navigation }) => {
     const [activeCategria, setActiveCategria] = useState(1);
     const pesquisado = produts.filter((data) => data.name.toLowerCase().includes(busca.toLowerCase()))
     return (
-        <View style={{ backgroundColor: LIGHT, flex: 1., bottom: 'auto' }}>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 30, padding: 15, }}>
-                <Ionicons name="grid" color={PRIMARY} size={20} />
-                <Text style={{ fontWeight: 'bold', fontSize: 15 }}>Explore</Text>
-                <Image source={user.profile} style={{ width: 50, height: 50, borderRadius: 15 }} />
-            </View>
+        <SafeAreaView style={{ backgroundColor: LIGHT, flex: 1., bottom: 'auto' }}>
 
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center', alignItems: 'center' }}>
-
-                <View style={{
-                    backgroundColor: PRIMARY, width: width - SPACING * 15, height: 50,
-                    padding: SPACING * 2, marginLeft: SPACING * 2, borderRadius: SPACING, flexDirection: 'row', alignContent: 'center', alignItems: 'center'
-                }}>
-                    <Ionicons name="search" color={LIGHT} size={20} />
-                    <TextInput value={busca} style={{ color: LIGHT }} onChangeText={(text) => setBusca(text)} placeholder="Find the best product for your pet" placeholderTextColor={LIGHT} />
-
+            <View >
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 30, padding: 15, }}>
+                    <Ionicons name="grid" color={PRIMARY} size={20} />
+                    <Text style={{ fontWeight: 'bold', fontSize: 15 }}>Explore</Text>
+                    <Image source={user.profile} style={{ width: 50, height: 50, borderRadius: 15 }} />
                 </View>
 
-                <TouchableOpacity style={{ backgroundColor: PRIMARY, borderRadius: SPACING, padding: SPACING * 3, marginRight: 7 }}>
-                    <Ionicons name="filter" color={LIGHT} size={20} />
-                </TouchableOpacity>
-            </View>
-            <View style={{ flexDirection: 'row', marginLeft: SPACING * 5, paddingTop: SPACING * 7 }}>
-                {AnimalCategory.map((data) =>
-                    <ScrollView key={data.id} horizontal>
-                        <TouchableOpacity key={data.id} onPress={() => setActiveCategria(data.id)} >
-                            <Text key={data.id} style={[{ color: GRAY, fontWeight: 'bold' }, activeCategria == data.id && { color: PRIMARY, fontWeight: '800' }]}>{data.name}</Text>
-                            <Text style={[activeCategria == data.id && { backgroundColor: ROSA, height: 4, borderRadius: SPACING, paddingTop: SPACING }]}></Text>
-                        </TouchableOpacity>
-                    </ScrollView>
-                )}
-            </View>
-            <ScrollView>
-                {pesquisado.map((data) =>
-                    <TouchableOpacity key={data.id} style={{
-                        padding: SPACING * 2,
-                        borderRadius: SPACING, marginTop: SPACING, backgroundColor:
-                            LIGHT, width: width - 18, height: 120, elevation: 2,
-                        marginLeft: SPACING * 2, marginRight: SPACING * 2,
-                        flexDirection: 'row'
-                    }} onPress={() => navigation.navigate('Product', data)}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center', alignItems: 'center' }}>
 
-                        <View>
-                            <Image source={data.Image} style={{ width: 100, height: 100 }} />
-                        </View>
-                        <View>
-                            <Text style={{ fontWeight: 'bold' }}>{data.name}</Text>
-                            <Text style={{ color: GRAY, width: 240, fontWeight: 'normal' }}>{data.description}</Text>
-                            <Text style={{ fontWeight: 'bold', color: GRAY }}>{data.price}.00MZN</Text>
-                        </View>
+                    <View style={{
+                        backgroundColor: PRIMARY, width: width - SPACING * 15, height: 50,
+                        padding: SPACING * 2, marginLeft: SPACING * 2, borderRadius: SPACING, flexDirection: 'row', alignContent: 'center', alignItems: 'center'
+                    }}>
+                        <Ionicons name="search" color={LIGHT} size={20} />
+                        <TextInput value={busca} style={{ color: LIGHT, marginLeft: SPACING * 2, width: 220 }} onChangeText={(text) => setBusca(text)} placeholder="Find the best product for your pet" placeholderTextColor={LIGHT} />
+                        {!busca ? <></> :
+
+                            <TouchableOpacity onPress={() => setBusca('')} style={{ left: SPACING * 52, position: 'absolute', width: 36, padding: SPACING, justifyContent: 'center' }}>
+                                <Ionicons name="close" color={LIGHT} size={SPACING * 4} />
+                            </TouchableOpacity>
+                        }
+                    </View>
+                    <TouchableOpacity style={{ backgroundColor: PRIMARY, borderRadius: SPACING, padding: SPACING * 3, marginRight: 7 }}>
+                        <Ionicons name="filter" color={LIGHT} size={20} />
                     </TouchableOpacity>
-                )}
-            </ScrollView>
-        </View>
+                </View>
+                <View style={{ flexDirection: 'row', marginLeft: SPACING * 5, paddingTop: SPACING * 7 }}>
+                    {AnimalCategory.map((data) =>
+                        <ScrollView key={data.id} horizontal>
+                            <TouchableOpacity key={data.id} onPress={() => setActiveCategria(data.id)} >
+                                <Text key={data.id} style={[{ color: GRAY, fontWeight: 'bold' }, activeCategria == data.id && { color: PRIMARY, fontWeight: '800' }]}>{data.name}</Text>
+                                <Text style={[activeCategria == data.id && { backgroundColor: ROSA, height: 4, borderRadius: SPACING, paddingTop: SPACING }]}></Text>
+                            </TouchableOpacity>
+                        </ScrollView>
+                    )}
+                </View>
+                <ScrollView>
+                    {pesquisado.map((data) =>
+                        <TouchableOpacity key={data.id} style={{
+                            padding: SPACING * 2,
+                            borderRadius: SPACING, marginTop: SPACING, backgroundColor:
+                                LIGHT, width: width - 18, height: 120, elevation: 2,
+                            marginLeft: SPACING * 2, marginRight: SPACING * 2,
+                            flexDirection: 'row'
+                        }} onPress={() => navigation.navigate('Product', data)}>
+
+                            <View>
+                                <Image source={data.Image} style={{ width: 100, height: 100 }} />
+                            </View>
+                            <View>
+                                <Text style={{ fontWeight: 'bold' }}>{data.name}</Text>
+                                <Text style={{ color: GRAY, width: 240, fontWeight: 'normal' }}>{data.description}</Text>
+                                <Text style={{ fontWeight: 'bold', color: GRAY }}>{data.price}.00MZN</Text>
+                            </View>
+                        </TouchableOpacity>
+                    )}
+                </ScrollView>
+            </View>
+        </SafeAreaView>
     )
 }
